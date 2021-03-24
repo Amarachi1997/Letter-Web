06b53ecb485d67fd9ccf61d49024c042c9e7eb40
Amara Chiaha <catherinechiaha@gmail.com>
2021-03-24 23:36:38 +0100

    Hone Component

--- components/Navigation.js
+++ components/Navigation.js
@@ -4,7 +4,7 @@ app.component('nav-section', {
     ` 
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
-      <a class="navbar-brand fs-1" href="#">Letter <br> Likho</a>
+      <a class="navbar-brand fs-1" href="#">Letter <br> Likho.com</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon "></span>
       </button>
