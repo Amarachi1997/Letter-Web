e4496ecb2d99081b3eca4bab9bdc735b0612a4de
Amara Chiaha <catherinechiaha@gmail.com>
2021-03-24 17:17:48 +0100

    Sass commit

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,32 @@
 app.component('footer-section', {
     template:
     /*html*/
-    ` `
+    ` 
+    <div id="footer">
+    <div class="container">
+        <!-- Stack the columns on mobile by making one full-width and the other half-width -->
+        <div class="row ">
+            <div class="col-8">
+                <h2>Letter <br> Likho.com</h2>
+                <span>copyright&#169; 2021 by Tajwat Designs all rights reserved</span>
+          
+            </div>
+            <div class="col-4">
+              <div class="footer__links">
+                <a href="#">Home</a>
+                <a href="#">About Us</a>
+                <a href="#">Blog</a>
+              </div>
+              <div class="footer__icons">
+               
+                <i class="fa fa-facebook-f fs-5"></i>
+                <i class="fa fa-twitter fs-5"></i>
+                <i class="fa fa-instagram fs-5"></i>
+                <i class="fa fa-linkedin fs-5"></i>
+              </div>
+            </div>
+          </div>
+</div>
+
+    `
 })
